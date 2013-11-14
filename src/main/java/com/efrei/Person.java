3f1e74b2b951e9e77a6d3578af64d89e71f32a41
charroux <benoit.charroux@free.fr>
2013-11-14 11:58:37 +0100

    first commit

---
+++ src/main/java/com/efrei/Person.java
@@ -0,0 +1,44 @@
+package com.efrei;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+
+
+@Entity
+public class Person {
+
+	int id;
+	String name;
+	
+	
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Id
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static void main(String [] a){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+		EntityManager entityManager = emf.createEntityManager();
+		
+		Person p = new Person();
+		entityManager.persist(p);
+	}
+	
+	
+}
