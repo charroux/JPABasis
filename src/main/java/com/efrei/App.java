8c045caaaeb47972569b738f7760e10c75255906
unknown <ben@pc-charroux.(none)>
2014-10-27 11:19:01 +0100

    first commit

---
+++ src/main/java/com/efrei/App.java
@@ -0,0 +1,26 @@
+package com.efrei;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+		EntityManager entityManager = emf.createEntityManager();
+		
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+		Person p = new Person();
+		entityManager.persist(p);
+		tx.commit();
+    }
+}
