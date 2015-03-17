88b02b91d12b1356df4ed76997b45a77be266357
charroux <benoit.charroux@free.fr>
2015-03-17 13:28:45 +0100

    first commit

--- src/main/java/com/efrei/Person.java
+++ src/main/java/com/efrei/Person.java
@@ -1,14 +1,9 @@
 package com.efrei;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Person {
@@ -16,29 +11,24 @@ public class Person {
 	int id;
 	String name;
 	
-	Set<Rent> rents = new HashSet<Rent>();
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-	@OneToMany(cascade=CascadeType.ALL,mappedBy="person")
-	public Set<Rent> getRents() {
-		return rents;
-	}
-	public void setRents(Set<Rent> rents) {
-		this.rents = rents;
-	}
+	
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + "]";
